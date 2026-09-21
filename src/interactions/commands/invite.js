2450ea5d68bd278a61702092227de6eb86a4b0f3
zt3xdv <zstr3mx.dev@gmail.com>
2026-09-21 12:38:17 -0300

    invite doesnt have inviter and expiration date always

--- src/interactions/commands/invite.js
+++ src/interactions/commands/invite.js
@@ -77,8 +77,8 @@ export default {
             {
               type: ComponentType.TextDisplay,
               content:
-                `-# ${getEmoji("invite", client)} **Inviter**: **${escapeMarkdown(data.inviter.global_name)}** @${escapeMarkdown(data.inviter.username)}${data.inviter.discriminator != 0 ? "#" + data.inviter.discriminator : ""} \`${data.inviter.id}\`\n` +
-                `-# ${getEmoji("calendar", client)} **Expires at**: ${formatDiscordDate(data.expires_at)}`
+                (data.inviter ? `-# ${getEmoji("invite", client)} **Inviter**: **${escapeMarkdown(data.inviter.global_name)}** @${escapeMarkdown(data.inviter.username)}${data.inviter.discriminator != 0 ? "#" + data.inviter.discriminator : ""} \`${data.inviter.id}\`\n` : "") +
+                (data.expires_at ? `-# ${getEmoji("calendar", client)} **Expires at**: ${formatDiscordDate(data.expires_at)}` : "")
             },
           ],
         },
